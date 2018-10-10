5f173c29a456d5708d837a505cdc22100274a9fd
Bennie van der Walt <barendjohannes.vdwalt@gmail.com>
2018-10-10 17:22:46 +0200

    Check that we can retrieve a single rebus
    Attempt to increase code coverage

--- tests/rebus.spec.js
+++ tests/rebus.spec.js
@@ -10,4 +10,13 @@ describe('Tests for rebuses', () => {
       expect(rebus.isAnswered).toEqual(false);
     });
   });
+  
+  it('Can retrieve a single rebus', () => {
+    const rebus = getRebus(1);
+    expect(typeof rebus.id).toEqual('number');
+    expect(typeof rebus.symbols[0]).toEqual('string');
+    expect(typeof rebus.words[0]).toEqual('string');
+    expect(rebus.input.length).toEqual(rebus.words.join('').length);
+    expect(rebus.isAnswered).toEqual(false);
+  });
 });
