3e48242bdfd026f4b7a8a82032762da714e2a170
Jakub Walczak <jakub.s.walczak@gmail.com>
2018-10-13 03:59:31 +0200

    Fix #65 - Add Babel plugin to prevent ReferenceError

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = api => {
   api.cache(true);
 
   return {
-    presets: [['@babel/preset-env', { targets: '> 1%, not dead' }]]
+    presets: [['@babel/preset-env', { targets: '> 1%, not dead' }]],
+    plugins: ['@babel/plugin-transform-runtime']
   };
 };
