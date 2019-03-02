d4e6a4cf9d6314f3983976b4c46f341e5280e907
Al Vincent Valdez <alvin.valdez.1111@gmail.com>
2019-03-01 19:27:42 -0500

    Added percent bar

---
+++ src/js/components/PercentBar.js
@@ -0,0 +1,20 @@
+import { createComponent } from '../mini';
+import { connect } from '../store';
+
+export function PercentBar(props) {
+  return connect(
+    createComponent({
+      props,
+      render({ rebuses }) {
+        const answeredRebusCount = rebuses.filter(rebus => rebus.isAnswered).length;
+        const percent =  Math.round(((answeredRebusCount/rebuses.length)*100)*10)/10;
+        
+
+        return ` 
+        <span class ="percent-bar" id="myspan"> ${percent}% </span>
+       
+        `;
+      }
+    })
+  );
+}
