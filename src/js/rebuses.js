8e32ff735a7af9ece57fc2f2d16c793e42b394c9
David <dpadillaf@unicartagena.edu.co>
2019-04-02 15:02:21 -0500

    new rebus added

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -870,6 +870,11 @@ const rebuses = [
     symbols: ['🏹', '+', '👔'],
     words: ['bow', 'tie'],
     hint: ['An accessory that is often worn with a suit']
+  },
+  {
+    symbols: ['🌎', 'OF', '⚔', 'CRAFT'],
+    words: ['World', 'of', 'Warcraft'],
+    hint: ['Famous Blizzard online game']
   }
 ];
 
