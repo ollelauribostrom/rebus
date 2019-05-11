be8f15a5f21e568a843248d472d96a0667a82105
John Gregory Funtera <gregory.funtera@gmail.com>
2019-05-12 00:21:44 +0800

    Added a new rebus Ghostbusters

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -870,6 +870,11 @@ const rebuses = [
     symbols: ['🏹', '+', '👔'],
     words: ['bow', 'tie'],
     hint: ['An accessory that is often worn with a suit']
+  },
+  {
+    symbols: ['👻', '+', '❌'],
+    words: ['Ghostbusters'],
+    hint: ['Who you gonna call?']
   }
 ];
 
