51eadb0a5ac6688971e74fd337d2c3cfafd73a73
Kenneth Blake Sweezy <33377824+KennethSweezy@users.noreply.github.com>
2019-05-19 14:16:14 -0400

    Update rebuses.js
    Added Handle. (🖐️+le)

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🖐️', '+', 'LE'],
+    words: ['handle'],
+    hint: ['A nickname by which something is known.']
   }
 ];
 
