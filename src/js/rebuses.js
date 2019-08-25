9998539548f6b2749a0247b0591d8d643d2bc1bb
Elgist <54081389+Elgist@users.noreply.github.com>
2019-08-25 02:02:01 +0200

    Update rebuses.js

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -888,6 +888,11 @@ const rebuses = [
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
   }
+  {
+    symbols: ['🐱', '+' '👀'],
+    words: ['cat', 'eyes'],
+    hint: ['An old japanesse animation manga']
+  }
 ];
 
 export function isRebusAnswered(id) {
