903b5a9ae6960412bbd016e490a6c7c7f3e4dbcc
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-10-01 02:12:28 +0530

    Added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🙆', '+', '☎️'],
+    words: ['Headphone'],
+    hint: 'Wireless over-the-ear _____ with extra bass!'
   }
 ];
 
