5cdfd357a66eee81b53270156dcf642d9f89c1ed
Ela Bogucka <ebogucka@users.noreply.github.com>
2019-09-30 22:51:11 +0200

    Add a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['☕', '+', '🪑'],
+    words: ['teacher'],
+    hint: ['Someone you learn from.']
   }
 ];
 
