188f4c1c539a2df5e85209f11d165cab6e1c1710
Zowac <kevin.vanderstracten@izi-solutions.fr>
2019-10-02 06:57:43 +0200

    new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🖊️', '+', '🌲', '🍎', '+', '🍎', '+', '🖊️'],
+    words: ['penpineappleapplepen'],
+    hint: ["Pikotaro's song"]
   }
 ];
 
