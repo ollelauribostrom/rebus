f33917821a8248fd546f722916e11fb494a7c555
ayush rahangdale <ayush22081993@gmail.com>
2019-10-04 11:07:08 +0530

    Added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,7 +887,12 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
-  }
+  },
+  {
+    symbols: ['💛','💌'],
+    words: ['love','letter'],
+    hint: ['I love you']
+  },
 ];
 
 export function isRebusAnswered(id) {
