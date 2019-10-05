a296af5af558bc65b4340b9d04204abe65439af5
zoraizasif <zoraizasif9@gmail.com>
2019-10-05 16:33:48 +0500

    Added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['R', '+', '❄️'],
+    words: ['rice'],
+    hint: ['It is the most widely consumed staple food']
   }
 ];
 
