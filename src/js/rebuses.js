f6563f0ea31e3474936b81907b9f86fada38c863
zoraizasif <zoraizasif9@gmail.com>
2019-10-05 13:44:17 +0500

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🐈', '-', 'T', '+', 'R'],
+    words: ['car'],
+    hint: ['Consits of wheels']
   }
 ];
 
