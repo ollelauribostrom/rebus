3c201d3cd04aee2b0de7ec9a4aab69e50dbd9462
garrettmasters <garrett.masters@fiftyandfifty.org>
2019-10-08 23:46:53 -0700

    Added new rebus Anchorage Associated Issue: #121

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['⚓', '+', '🎂'],
+    words: ['Anchorage'],
+    hint: ['A place in Alaska']
   }
 ];
 
