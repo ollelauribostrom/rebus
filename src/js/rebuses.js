c34bd9d4f84ac7e84f31f97b191407c2f4376f46
MFGookey <MFGookey@users.noreply.github.com>
2019-10-11 13:41:59 -0400

    Added family tree rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['👨‍👩‍👧‍👦', '+', '🌴'],
+    words: ['family', 'tree'],
+    hint: ['A graph of your ancestors']
   }
 ];
 
