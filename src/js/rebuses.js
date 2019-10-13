2451d335fce05d336b0a1288cc11534c5a30925f
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2019-10-14 00:13:30 +0300

    add rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['S', '🌳', 'T'],
+    words: ['street'],
+    hint: ['A public road.']
   }
 ];
 
