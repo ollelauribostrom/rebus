09cdb14e82217c843db054cad4b6294258b76c66
Beau Elstgeest <beau.elstgeest@gmail.com>
2019-10-15 22:57:56 +0200

    add rebus rave

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🌊 W = R']
+    words: ['rave']
+    hint: ['Dance like nobody is watching']
   }
 ];
 
