8a2486f1f6ac11d639528c1caa705f04e0d0eb11
Jay Khatri <38436564+jayskhatri@users.noreply.github.com>
2019-10-19 02:16:04 +0530

    Added blackberry

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['⬛', '+', '🍒'],
+    words: ['blackberry'],
+    hint: ['They are red when they are green']
   }
 ];
 
