991fe1242b57b68b73b8f9a9bbf385c57a852c22
Ayush Jain <ayushjain9501@gmail.com>
2019-10-23 11:32:45 +0530

    added rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🌧️', '+', '✔'],
+    words: ['Rain', 'check'],
+    hint: ['A coupon given for later use.']
   }
 ];
 
