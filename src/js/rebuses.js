b9954c46e764a8b65b0747ef40a8bb247e7250a6
Chris Vick <cvick@users.noreply.github.com>
2019-10-24 10:12:51 -0700

    add new rebus 'anteater'

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🐜', '+', '🍴', '+', 'er'],
+    words: ['anteater'],
+    hint: ['An animal with a long nose that eats small insects.']
   }
 ];
 
