028b7146600f7a9a93be0cb9882b7709e9e13edc
Hash_Code <shadrack.limbua@gmail.com>
2019-10-30 06:47:50 +0100

    My first rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🌧', '+', '🙇'],
+    words: ['Rain', 'Bow'],
+    hint: 'Colors in the sky'
   }
 ];
 
