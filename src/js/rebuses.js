df3e4933e3486adfba1974e6bfe9b41826f6f665
Chinmay Atrawalkar <catrawalkar@gmail.com>
2019-10-31 12:10:45 +0530

    'new_rebus_ONEPLUS

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['1️⃣', '+', '➕'],
+    words: ['OnePlus'],
+    hint: ['A Tech Brand.']
   }
 ];
 
