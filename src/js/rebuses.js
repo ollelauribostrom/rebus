5887a4bc4ae71e3a78d874d35535b681321f6fa6
NateOsterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2019-10-31 23:26:51 -0400

    add rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🏫', '+', '🚌'],
+    words: ['school', 'bus'],
+    hint: ['Think of a giant Twinky with wheels']
   }
 ];
 
