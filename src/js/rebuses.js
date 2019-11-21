35cfc1178849310545e0b60a1c03e707032969bb
tikhsuP <42881874+tikhsuP@users.noreply.github.com>
2019-11-21 19:07:35 +0530

    Added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🦇', '+', '👨'],
+    words: ['Batman'],
+    hint: "Joker's enemy, billionaire, Gotham's hope."
   }
 ];
 
