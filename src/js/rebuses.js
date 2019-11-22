689bf0454d6ce49d2b25fb73fc6cdb3340f4570b
tikhsuP <42881874+tikhsuP@users.noreply.github.com>
2019-11-22 21:08:38 +0530

    Add rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['Iron', '+', '👨'],
+    words: ['Ironman'],
+    hint: 'We love you 3000'
   }
 ];
 
