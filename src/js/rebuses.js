3bd663d76a177232ad00aa98e55a6fec1a21b286
Oswld <user.oswld@gmail.com>
2020-01-14 18:51:47 -0600

    Add new rebus-cat

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['😺', '+', '💻'],
+    words: ['GitHub'],
+    hint: ['Where software is built']
   }
 ];
 
