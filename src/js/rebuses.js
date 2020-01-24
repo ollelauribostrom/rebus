d587b120289da0f4d3ab1c408a870cd97559f78b
daniel.k <Kalyanasundaram.Vela@rci.rogers.ca>
2020-01-24 02:52:11 -0500

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['game', '+', 'of thrones'],
+    words: ['GOT'],
+    hint: ['A famous series']
   }
 ];
 
