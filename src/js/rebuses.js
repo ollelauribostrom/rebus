99eed7e37b1835c7b862d6bed102771a49e3b6f8
Gustaf Holst <guho1700@student.miun.se>
2020-01-24 19:22:25 +0100

    Adding a juicy rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🔓', '🍋', '+', 'ce'],
+    words: ['open', 'source'],
+    hint: ['The kind of software we all like']
   }
 ];
 
