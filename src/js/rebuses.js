503f9673984bac03aba485d50ee0d3beefc82b4b
Urtea <coffee.post@gmail.com>
2020-02-12 11:06:45 +0200

    Added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+    {
+    symbols: ['🌞', '+', '💡'],
+    words: ['Sunlight'],
+    hint: ['Makes the day bright']
   }
 ];
 
