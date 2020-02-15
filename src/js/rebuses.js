7013e5974452d559b19a2e88c55a851d233e3ef4
hideki <hideckies@gmail.com>
2020-02-15 20:50:44 +0900

    Add new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['⏲', '+', '💣'],
+    words: ['timebomb'],
+    hint: ['Hurry up! Otherwise it will explode.']
   }
 ];
 
