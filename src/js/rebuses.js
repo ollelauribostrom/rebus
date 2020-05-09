90f0fe6c7bd891341e5e362ca6f14781ca443705
Shivaram Gopalakrishnan <shivramgopal93@gmail.com>
2020-05-09 15:21:58 -0400

    Adding a new redbus #179

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['#FFFFFF', '+', '🏠'],
+    words: ['whitehouse'],
+    hint: ['The residence of President of USA']
   }
 ];
 
