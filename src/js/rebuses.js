c04d413590ed34a90fe2069971e336b4c9ac79ba
rajamanojkumar <rajamanoj400@gmail.com>
2020-06-29 20:51:37 +0530

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+{
+    symbols: ['👶', '+', '🏦'],
+    words: ['kidzbank'],
+    hint: ['A bank for kidz.']
   }
 ];
 
