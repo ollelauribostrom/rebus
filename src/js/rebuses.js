de1d146ff8edd5d92bd9edde1375685b616e2e9c
Jake Simmens <rocknjake@gmail.com>
2020-07-06 16:08:29 -0500

    Add new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🥄', '+', '⋔'],
+    words: ['spork'],
+    hint: ['Two utensils had a baby']
   }
 ];
 
