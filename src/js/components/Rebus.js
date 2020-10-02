95ba6a359e5e5f24c73fae6b7350afb7b13e5e07
Martin Hoffmann <>
2020-10-02 23:24:15 +0200

    Added difficulty

--- src/js/components/Rebus.js
+++ src/js/components/Rebus.js
@@ -1,23 +1,7 @@
 import { createComponent } from '../mini';
 import { connect } from '../store';
 import { Word } from './Word';
-
-function getDifficulty(rebus) {
-  switch (rebus.difficulty) {
-    case 1:
-      return 'easy';
-    case 2:
-      return 'basic';
-    case 3:
-      return 'intermediate';
-    case 4:
-      return 'hard';
-    case 5:
-      return 'very hard';
-    default:
-      return '';
-  }
-}
+import { getDifficulty } from '../rebuses';
 
 export function Rebus(props, ...children) {
   return connect(
--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -1098,3 +1098,20 @@ export function getRebuses() {
     };
   });
 }
+
+export function getDifficulty(rebus) {
+  switch (rebus.difficulty) {
+    case 1:
+      return 'easy';
+    case 2:
+      return 'basic';
+    case 3:
+      return 'intermediate';
+    case 4:
+      return 'hard';
+    case 5:
+      return 'very hard';
+    default:
+      return '';
+  }
+}
