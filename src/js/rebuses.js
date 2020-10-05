765cc8208050f6e3bc1330b398c61e12898cf61d
2knal <kunal.sonawane@somaiya.edu>
2020-10-05 19:02:42 +0530

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+	symbols: ['🦶', '+', '⚽'],
+	words: ['football'],
+	hint: ['Among the most followed sport in the world.']
   }
 ];
 
