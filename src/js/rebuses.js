ca5e13e40140747fb7fa049828f2df0d76d5f0cb
Dviti Arora <dvitiarora125btcse18@igdtuw.ac.in>
2020-10-06 19:20:46 +0530

    Adding Rebus Carpool

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['🚗', '+', '🏊'],
+    words: ['carpool'],
+    hint: ['An environment friendly way to commute/travel.']
   }
 ];
 
