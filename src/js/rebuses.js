3e744a3851e489e0e7efe6498d5e1e001c283cab
haithamsha <shahaitham@gmail.com>
2020-10-09 20:05:22 +0200

    add game of thrones rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['⚽', 'OF', '⚔'],
+    words: ['Game', 'of', 'thrones'],
+    hint: ['Winter is comming!']
   }
 ];
 
