87838b879411c5bc2dd4e04bf7b114fd7823abb6
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2020-10-31 21:25:31 -0300

    add a new rebus option to rebuses array

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['⚔️', '+', '🎨', '+', 'on', '-----'],
+    words: ['Sword', 'art', 'online'],
+    hint: ['A popular japanese light novel and anime']
   }
 ];
 
