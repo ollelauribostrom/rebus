73f869d55695310e838b35a8aa255836a340aa24
Muhammad Hamza <71731160+MD-Hamza@users.noreply.github.com>
2020-12-24 02:31:51 -0500

    Update rebuses.js

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -887,6 +887,11 @@ const rebuses = [
     symbols: ['🍯', '+', '🐝'],
     words: ['honeybee'],
     hint: ['An insect that makes a sweet treat.']
+  },
+  {
+    symbols: ['👂', '+', '🥁'],
+    words: ['eardrum'],
+    hint: ["Part of your ear that vibrates"]
   }
 ];
 
