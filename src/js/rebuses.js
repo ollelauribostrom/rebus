52fea1bb1c33e22b7a6003dc3fd2e6ccdd0be0d2
MAG-BOSS <b18025@students.iitmandi.ac.in>
2021-01-05 14:57:02 +0530

    added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🧱', '+', '⏱️'],
+    words: ['wall', 'clock'],
+    hint: 'a time-showing instrument'
   }
 ];
 
