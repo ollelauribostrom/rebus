d5f154abb96cc22b09f5ea15c3520a14795c44f1
rafael9martins <70760015+rafael9martins@users.noreply.github.com>
2021-02-28 08:21:27 -0300

    Refactoring Rebus in Portuguese (Brazil) and Reset Button v20

--- src/js/appBR.js
+++ src/js/appBR.js
@@ -1,23 +1,5 @@
 import { clickReset } from './components/ResetButton';
 
-function mouseHoverPTBR(e) {
-  e.addEventListener('mouseover', () =>
-    document.getElementById('button-text-id-br').classList.add('button-text-show')
-  );
-  e.addEventListener('mouseout', () =>
-    document.getElementById('button-text-id-br').classList.remove('button-text-show')
-  );
-}
-
-function mouseHoverEN(a) {
-  a.addEventListener('mouseover', () =>
-    document.getElementById('button-text-id-en').classList.add('button-text-show')
-  );
-  a.addEventListener('mouseout', () =>
-    document.getElementById('button-text-id-en').classList.remove('button-text-show')
-  );
-}
-
 // BUTTON [PT-BR] - LOAD BUTTON
 export function clickbr() {
   // CONDITION: if init as [pt-br] rebus, change design and hide button [ptbr]
@@ -25,7 +7,12 @@ export function clickbr() {
   checkIsPTBR();
   const e = document.getElementById('button-ptbr'); // get button [pt-br]
   // HOVER FUNCTION: cursor (hover) [pt-br]
-  mouseHoverPTBR(e);
+  e.addEventListener('mouseover', () =>
+    document.getElementById('button-text-id-br').classList.add('button-text-show')
+  );
+  e.addEventListener('mouseout', () =>
+    document.getElementById('button-text-id-br').classList.remove('button-text-show')
+  );
   // CLICK FUNCTION: click on button [pt-br]
   e.addEventListener('click', () => localStorage.setItem('flagBR', 'true'));
   // FLAG: now [pt-br] rebus
@@ -35,7 +22,12 @@ export function clickbr() {
 export function clicken() {
   const a = document.getElementById('button-en'); // get button [english]
   // HOVER FUNCTION: cursor (hover) [english]
-  mouseHoverEN(a);
+  a.addEventListener('mouseover', () =>
+    document.getElementById('button-text-id-en').classList.add('button-text-show')
+  );
+  a.addEventListener('mouseout', () =>
+    document.getElementById('button-text-id-en').classList.remove('button-text-show')
+  );
   // CLICK FUNCTION: click on button [english]
   a.addEventListener('click', () => localStorage.setItem('flagBR', 'false'));
   // FLAG: now [english / original] rebus
