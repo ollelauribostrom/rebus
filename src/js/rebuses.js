6d3e2ee221f2ad9e6a911074e8ba226bdd3d6caa
sivakrish-45 <sivaramakrishnan@student.tce.edu>
2021-04-03 02:46:21 -0700

    added rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🖊', '+', '👸'],
+    words: ['penguin'],
+    hint: ['bird that cannot fly']
   }
 ];
 
