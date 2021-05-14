d2c7296f7c1eb31a70adaa2771bb2133293aee66
William <82436358+ochowei@users.noreply.github.com>
2021-05-14 14:39:01 +0800

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['X', '+', '👨👨👨'],
+    words: ['Xmen'],
+    hint: 'Mutation'
   }
 ];
 
