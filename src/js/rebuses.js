41319f71a53aade0d130c1565fbb67d4e7603e30
Sierra <sierra.acy@gmail.com>
2021-06-04 10:11:42 -0700

    adding new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🕳️', '+', '🌊'],
+    words: ['holy', 'water'],
+    hint: 'Used for spiritual cleansing'
   }
 ];
 
