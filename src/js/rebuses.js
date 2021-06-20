97c0d321e766c1ce7465eeb919532791d93e4de1
divincode <devadiga.1@iitj.ac.in>
2021-06-20 11:19:49 +0530

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🐉', '+', '⚽'],
+    words: ['dragonball'],
+    hint: 'A famous anime series'
   }
 ];
 
