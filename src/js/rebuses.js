dc0cf8507668fe0fd9296f1b696b814cf190088a
Michal Sapinski <Sapinski.Michal@gmail.com>
2021-07-12 02:20:03 +0200

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🐜', '+', 'i', '+', '.e'],
+    words: ['antidote'],
+    hint: 'A remedy for poisoning'
   }
 ];
 
