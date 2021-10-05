be1dc1c776b357e73474ae98f2a398ae9c0c1016
j1in <jerry.yfl29@gmail.com>
2021-10-05 14:39:10 +1000

    Adding kingdom rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🤴️', '+', 'dom'],
+    words: ['kingdom'],
+    hint: 'A king rules over his ...'
   }
 ];
 
