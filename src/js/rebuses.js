f61a9e2b61184b4e71dd3aa91f93478f40eb7442
Osman Yasir Çankaya <75441186+osmanyasircankaya@users.noreply.github.com>
2021-10-10 14:12:17 +0300

    new rebus added

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🦶', '+', '⚽'],
+    words: ['football'],
+    hint: 'Most famous sport in the world'
   }
 ];
 
