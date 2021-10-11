4dc5e3e07cb2906e66e8895f703af838ad649393
RockosBos <32206608+RockosBos@users.noreply.github.com>
2021-10-10 22:08:46 -0400

    Added new rebus - catnap

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['😺', '+' + '💤'],
+    words: ['catnap'],
+    hint: 'A short period of sleep'
   }
 ];
 
