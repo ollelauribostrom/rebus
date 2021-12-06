627536a6b0b28bde93d0ff3ad37174f0a7fcb948
josephmbarron <joseph.barron@ufl.edu>
2021-12-06 21:02:46 +0000

    Add hint to 'rebuses' object

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  }, 
+  {
+    symbols: ['🍎', '+', '🥧'],
+    words: ['apple', 'pie'], 
+    hint: 'A classic American comfort food'
   }
 ];
 
