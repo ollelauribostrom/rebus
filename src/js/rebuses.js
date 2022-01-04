9c0e9f7c44df5c46d7ec53f859c851586c512bbb
dilan <d.adel@student.rug.nl>
2022-01-05 00:50:46 +0100

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['✖️', '+', '⬛'],
+    words: ['times', 'square'],
+    hint: 'Famous place'
   }
 ];
 
