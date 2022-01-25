5d814dc8ecc6f46762dd67e467063a323e25f0ba
mukul ojha <mukulsharma@gmail.com>
2022-01-25 14:13:30 +0530

    added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🚧', '+', '🔗'],
+    words: ['block', 'chain'],
+    hint: 'A technology'
   }
 ];
 
