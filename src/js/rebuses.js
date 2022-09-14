f56f5cbe3c0cdd6e3f08c8ef3ece3da2aa4b0b7b
Virus2466 <infgvirus@gmail.com>
2022-09-14 19:25:01 +0530

    Dependencies and new rubus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🎮' , '+' , '  '],
+    words: ['Game' , 'Dev'],
+    hint: 'Person Who Makes Game'
   }
 ];
 
