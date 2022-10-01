527cfb0ccdbd744924b4bb546e8599076a45732c
Gokul Kumar R <rsgokulkumar@gmail.com>
2022-10-01 19:21:24 +0530

    Added rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🚗', '@'],
+    words: ['Carrot'],
+    hint: 'What does Bugs Bunny like ?'
   }
 ];
 
