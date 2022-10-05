a180b4781f974e58c51430662c99cf6ad869844e
Lakhwinderr <lakhwinder.sidhuu20@gmail.com>
2022-10-05 19:06:30 +0530

    added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -5,6 +5,11 @@ const rebuses = [
     hint: 'You´re solving one right now'
   },
   {
+    symbols: ['D', '+', '🐛'],
+    words: ['Debug'],
+    hint: 'Fixing errors in your code'
+  },
+  {
     symbols: ['🏠', '+', 'pl', '+', '🐜', '+', 's'],
     words: ['Houseplants'],
     hint: `The second emoji is 'ant' not 'bug'`
