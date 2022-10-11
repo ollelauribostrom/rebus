3c567feeb13925ea1c1d7c28cb6f1ab14537b19f
bhaskar metiya <bhaskarmetiya@gmail.com>
2022-10-11 23:57:01 +0530

    adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🆚', '+', '🧑‍💻'],
+    words: ['vs', 'code'],
+    hint: 'A code editor'
   }
 ];
 
