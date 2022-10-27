dd59e11be60513bba4efc12e3d089fea8b57661f
Mohamed <metzopro97@gmail.com>
2022-10-27 16:15:00 +0200

    play game in a chosen language

---
+++ src/js/frenchRebuses.js
@@ -0,0 +1,17 @@
+export const frenchRebuses = [
+  {
+    symbols: ['Re', '+', '🚌'],
+    words: ['Rebus'],
+    hint: 'You´re solving one right now'
+  },
+  {
+    symbols: ['🍲', '+', '🧓'],
+    words: ['Potager'],
+    hint: 'You´re solving one right now'
+  },
+  {
+    symbols: ['⭐', '+', '🐠'],
+    words: ['Starfish'],
+    hint: 'Say the two emojis out loud'
+  }
+];
