8d1048cc08bf16e4071d54a6ad42367ea28e766f
Andi <andililajal@gmail.com>
2022-11-19 09:23:58 +0100

    Added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['©', '+', '🔪'],
+    words: ['C', 'sharp'],
+    hint: 'A programming language'
   }
 ];
 
