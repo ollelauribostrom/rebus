3e0bb37abf72d1c2293d9bf378d5748d512c03fb
Mohamed <metzopro97@gmail.com>
2023-01-02 00:30:41 +0100

    Adding french rebuses

--- src/js/frenchRebuses.js
+++ src/js/frenchRebuses.js
@@ -2,16 +2,36 @@ export const frenchRebuses = [
   {
     symbols: ['Re', '+', '🚌'],
     words: ['Rebus'],
-    hint: 'You´re solving one right now'
+    hint: 'Tu as résolu le premier maintenant'
   },
   {
     symbols: ['🍲', '+', '🧓'],
     words: ['Potager'],
-    hint: 'You´re solving one right now'
+    hint: 'jardin de légumes et fruits'
   },
   {
-    symbols: ['⭐', '+', '🐠'],
-    words: ['Starfish'],
-    hint: 'Say the two emojis out loud'
+    symbols: ['🥬', '+', '🌹'],
+    words: ['Choufleur'],
+    hint: 'Légume vert'
+  },
+  {
+    symbols: ['🥖', '+', '🪣'],
+    words: ['Pinceau'],
+    hint: 'outil utilisé par les peintres'
+  },
+  {
+    symbols: ['🎲', '+', '🪚', '+', '👃'],
+    words: ['Dessiner'],
+    hint: 'action de faire un dessin'
+  },
+  {
+    symbols: ['🚌', '+', '🐟'],
+    words: ['Carton'],
+    hint: 'un contenant'
+  },
+  {
+    symbols: ['&', '+', '🥛', '+', '🧤'],
+    words: ['Elegant'],
+    hint: 'etre elegant'
   }
 ];
