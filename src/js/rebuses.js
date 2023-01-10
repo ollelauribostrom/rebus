71e42ea51ac93f5f9b9079321006f29fec1751f1
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-01-10 13:38:25 +0100

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🌽', '+', 'row'],
+    words: ['cornrow'],
+    hint: 'A beautiful hairstyle'
   }
 ];
 
