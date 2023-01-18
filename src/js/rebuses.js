b26b3f47aa8ae68f60826f00c35fca303673b078
reenadangi <reena.dangi@gmail.com>
2023-01-17 23:47:56 -0600

    Cowboy added

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -5,6 +5,11 @@ const rebuses = [
     hint: 'You´re solving one right now'
   },
   {
+    symbols: ['🐮', '+', '👦'],
+    words: ['Cowboy'],
+    hint: `This is not actual cow`
+  },
+  {
     symbols: ['🏠', '+', 'pl', '+', '🐜', '+', 's'],
     words: ['Houseplants'],
     hint: `The second emoji is 'ant' not 'bug'`
