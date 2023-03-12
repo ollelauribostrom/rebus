f3b01a9aff3b1af2f7f5c70ab6c10aafaa366ed6
David <Davidkjensonii@gmail.com>
2023-03-11 17:45:26 -0800

    Added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols:['🧵', '+', '🧵', '+', '🧵'],
+    words :['multithread'],
+    hint: 'Parallel computing'
   }
 ];
 
