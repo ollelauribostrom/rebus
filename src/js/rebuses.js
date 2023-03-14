74ab3fee9a1d9921e5c3f2c91c98436c41036d7b
Kaif Khan <kaifkhan.biz@gmail.com>
2023-03-14 15:38:32 +0530

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols : ['😺', '+', '🥾'],
+    words : ['Puss', 'in', 'Boots'],
+    hint : ['Famous Animated Movie']    
   }
 ];
 
