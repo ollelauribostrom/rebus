19b4f6895a09cb9344d09e3b260624a312f76b2d
wasifdv <wasifakram962@gmail.com>
2023-06-20 12:36:07 +0530

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🚩', '+', '🎓'],
+    words: ['red', 'hat'],
+    hint: 'A type of software'
   }
 ];
 
