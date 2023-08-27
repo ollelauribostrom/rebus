9b9ec451eb490c85da3673987c000de604b4c5cc
Agnik Bakshi <77234005+Agnik7@users.noreply.github.com>
2023-08-27 20:37:12 +0530

    Add rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🙋', '+', 'TA' ],
+    words:['meta'],
+    hint: 'Facebook's new name'
   }
 ];
 
