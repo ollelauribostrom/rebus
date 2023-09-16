04e41e2ca08084a4956d02f1124a9dcd51e49d79
ankit-suman-07 <ankitsuman07@gmail.com>
2023-09-16 11:08:24 +0530

    Added a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['💼', '+', '🏖️'],
+    words: ['workation'],
+    hint: 'A much needed productive break.'
   }
 ];
 
