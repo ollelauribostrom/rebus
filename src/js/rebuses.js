d04a72cd292f36a7a58f11916ec69097c4fa530d
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-10-01 22:30:11 -0300

    Adding a new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🌎', '+', '🏹'],
+    words: ['world', 'little bow'],
+    hint: 'It makes sense'
   }
 ];
 
