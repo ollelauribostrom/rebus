f3aafa3399a696e44ff5a38c1c1229e4c4c51dfa
UmtHym <120595657+UmtHym@users.noreply.github.com>
2023-11-26 19:05:16 +0100

    Added Dragonball rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🐲', '+', '⚽', '+', 'Z'],
+    words: ['dragon', 'ball', 'z'],
+    hint: 'KAMEHAMEHA!'
   }
 ];
 
