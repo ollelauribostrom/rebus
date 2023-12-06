db61bf044b1d57590f466958b71e44c827582063
Aakash Soni <inos92@gmail.com>
2023-12-05 19:12:58 -0600

    added new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['🪝', '+', '🔑'],
+    words: ['hookey'],
+    hint: ['Skipping school']
   }
 ];
 
