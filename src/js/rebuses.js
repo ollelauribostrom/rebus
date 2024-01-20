b764a5c2cdcf103b83928424e4f4bf6c99762d81
Thoss24 <t.fogarty24@outlook.com>
2024-01-20 15:18:39 +0000

    Adding new rebus

--- src/js/rebuses.js
+++ src/js/rebuses.js
@@ -902,6 +902,11 @@ const rebuses = [
     symbols: ['🦀', '+', '🎂'],
     words: ['crab', 'cake'],
     hint: 'A delicious appetizer'
+  },
+  {
+    symbols: ['⭐', '+', '⚔', 's'],
+    words: ['Star Wars'],
+    hint: 'Luke, I am your Father'
   }
 ];
 
